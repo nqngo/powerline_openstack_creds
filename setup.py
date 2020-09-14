67f2b6595f047b37f2cc4fcd56e819721e03a422
Nhat Ngo <me@nqngo.com>
2020-09-15 00:13:50 +1000

    Set version to 0.1.0 for testing.

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ from setuptools import setup
 setup(
     name             = 'powerline-openstack-creds',
     description      = 'A Powerline segment for showing current sourced Openstack credential',
-    version          = '1.0.0',
+    version          = '0.1.0',
     keywords         = 'powerline openstack_creds_helper',
     license          = 'MIT',
     author           = 'Nhat Ngo',
     author_email     = 'me@nqngo.com',
     url              = 'https://github.com/nqngo/powerline-openstack_creds',
     packages         = ['powerline-openstack-creds'],
-    install_requires = ['powerline-status''],
+    install_requires = ['powerline-status'],
     classifiers      = [
         'Environment :: Console',
         'Intended Audience :: Developers',
